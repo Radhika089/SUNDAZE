6797f3346a329ae04b3d8badb2d28e404b7bc84f
Radhika <radhikabansal365@gmail.com>
2026-09-25 08:55:13 +0530

    feat: enhance sections with GSAP animations and improve ClipPathTitle component

--- sundaze/src/App.jsx
+++ sundaze/src/App.jsx
@@ -2,9 +2,13 @@ import { useGSAP } from "@gsap/react";
 import Navbar from "./components/Navbar";
 import FlavourSections from "./sections/FlavourSections";
 import Hero from "./sections/Hero";
+import gsap from "gsap";
 import MessageSection from "./sections/MessageSection";
 import NutritionSection from "./sections/NutritionSection";
 import BenefitSection from "./sections/BenefitSection";
+import { ScrollTrigger, ScrollSmoother } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
   useGSAP(() => {
--- sundaze/src/sections/BenefitSection.jsx
+++ sundaze/src/sections/BenefitSection.jsx
@@ -1,9 +1,98 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import VideoPinSection from "../components/VideoPinSection";
 import ClipPathTitle from "../components/ClipPathTitle";
 
 const BenefitSection = () => {
-  return <Div></Div>;
+  useGSAP(() => {
+    const revealT1 = gsap.timeline({
+      delay: 1,
+      scrollTrigger: {
+        trigger: ".benefit-section",
+        start: "top 60%",
+        end: "top top",
+        scrub: 1.5,
+      },
+    });
+
+    revealT1
+      .to(".benefit-section .first-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .second-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .third-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .fourth-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      });
+  });
+
+  return (
+    <section className="benefit-section" id="benefits">
+      <div className="container mx-auto pt-20">
+        <div className="col-center">
+          <p>
+            Unlock Advantages <br />
+            Explore the key Benefit of Choosing SPYLT
+          </p>
+          <div className="col-center mt-20">
+            <ClipPathTitle
+              title={"Shelf stable"}
+              color={"#faeade"}
+              bg={"#c88e64"}
+              className={"first-title"}
+              borderColor={"#222123"}
+            />
+
+            <ClipPathTitle
+              title={"Protein + Caffeine"}
+              color={"#222123"}
+              bg={"#faeade"}
+              className={"second-title"}
+              borderColor={"#222123"}
+            />
+
+            <ClipPathTitle
+              title={"Infinitely recyclable"}
+              color={"#faeade"}
+              bg={"#7F3B2D"}
+              className={"third-title"}
+              borderColor={"#222123"}
+            />
+
+            <ClipPathTitle
+              title={"Lactose free"}
+              color={"#2E2D2F"}
+              bg={"#FED775"}
+              className={"fourth-title"}
+              borderColor={"#222123"}
+            />
+          </div>
+          <div className="md:mt-0 mt-10">
+            <p>And much more....</p>
+          </div>
+        </div>
+      </div>
+      <div className="relative overlay-box">
+        <VideoPinSection />
+      </div>
+    </section>
+  );
 };
 
 export default BenefitSection;
